ee1dd508ebd1130b9f9968f842db32b8be4ca70f
Léo <51166933+SublimeLeo@users.noreply.github.com>
2019-09-24 18:11:39 +0200

    Updated Sublime Bid Adapter to v0.5.0 (#15)
    * Updated Sublime Bid Adapter to v0.5.0
    
    * Fixed callbackname and added comments
    
    * Fixed url-encoded data from notify + comments and comma
    
    * Added params.debug and mandatory pixel logic
    
    * Fix params debug
    
    * More verbose on pixel debug
    
    * Update isBidRequest validation and query string
    
    * Add missing return statement
    
    * Added pixels for bid and timeout
    
    * Unified sendpixel and sendevent
    
    * Made bid unique
    
    * Refactored code + unit tests

--- test/spec/modules/sublimeBidAdapter_spec.js
+++ test/spec/modules/sublimeBidAdapter_spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
-import { spec } from 'modules/sublimeBidAdapter.js';
-import { newBidder } from 'src/adapters/bidderFactory.js';
+import { spec } from 'modules/sublimeBidAdapter';
+import { newBidder } from 'src/adapters/bidderFactory';
 
 describe('Sublime Adapter', function() {
   const adapter = newBidder(spec);
@@ -70,17 +70,14 @@ describe('Sublime Adapter', function() {
 
     it('should have a post method', function() {
       expect(request[0].method).to.equal('POST');
-      expect(request[1].method).to.equal('POST');
     });
 
     it('should contains a request id equals to the bid id', function() {
       expect(request[0].data.requestId).to.equal(bidRequests[0].bidId);
-      expect(request[1].data.requestId).to.equal(bidRequests[1].bidId);
     });
 
     it('should have an url that contains bid keyword', function() {
       expect(request[0].url).to.match(/bid/);
-      expect(request[1].url).to.match(/bid/);
     });
   });
 
